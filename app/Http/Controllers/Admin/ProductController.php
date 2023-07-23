0059e4103e7858954d149d7d8b24a91a5197b285
Snow <thingoctuyettruong@gmail.com>
2023-07-23 11:38:22 +0700

    feat:crud product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,38 +3,43 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Product\ProductRequest;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Traits\ImageTrait;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use JetBrains\PhpStorm\NoReturn;
 
 class ProductController extends Controller
 {
+    use ImageTrait;
+    public int $itemPerPage = 10;
     public function index(): View
     {
-        $products = Product::all();
+        $products = Product::query()->orderByDesc('created_at')->paginate($this->itemPerPage);
         return view('admin.products.index', compact('products'));
     }
 
     public function create(): View
     {
         $categories = Category::all();
-       return view('admin.products.create', compact('categories'));
+       return view('admin.products.create', compact('categories') );
     }
 
-    public function store(Request $request)
+    public function store(ProductRequest $request): RedirectResponse
     {
-        $product = new Product();
-
-        $product->name = $request->input('name');
-        $product->category_id = $request->input('category_id');
-        $product->sku = $request->input('sku');
-        $product->stock = $request->input('stock');
-        $product->description = $request->input('description');
-        $product->original_price = $request->input('original_price');
-        $product->selling_price = $request->input('selling_price');
-        $product->original_price = $request->input('original_price');
-        $product->save();
-        return redirect('products')->with('status', "Category Added Successfully");
+        $data = $request->validated();
+        $data['image'] = $this->uploadImage($request, 'image', 'images');
+        Product::query()->create([
+            'name' => $data['name'],
+            'description' => $data['description'],
+            'stock' => $data['stock'],
+            'original_price' => $data['original_price'],
+            'selling_price' => $data['selling_price'],
+            'sku' => $data['sku'],
+            'category_id' => $data['category_id'],
+        ]);
+        return redirect('products')->with('status', 'Product Added Successfully');
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,33 +1,43 @@
 @extends("admin.layouts.app")
 @section("content")
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($categories as $item)
-                <tr>
-                    <th>{{$item->id}}</th>
-                    <td>{{$item->name}}</td>
-                    <td>{{$item->created_at}}</td>
-                    <td>
-                        <a class="btn btn-primary btn-sm" href="{{ route('category.edit', ['id' => $item->id]) }}">
-                            <i class="fa fa-pencil"></i>
-                        </a>
-                        <a href="{{ route('category.delete', ['id' => $item->id]) }}" class="btn btn-danger btn-sm">
-                            <i class="fa fa-trash"></i>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+    <div class="mb-3">
+        <a href="{{ route('product.create') }}" class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i> Create Product</a>
     </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($products as $item)
+                        <tr>
+                            <th>{{$item->id}}</th>
+                            <td>{{$item->name}}</td>
+                            <td>{{$item->category->name}}</td>
+                            <td>{{$item->created_at}}</td>
+                            <td>
+                                <a class="btn btn-primary btn-sm" href="#">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">
+                                    <i class="fa fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
 
+                {{ $products->links() }}
+            </div>
+        </div>
+    </div>
 @endsection
